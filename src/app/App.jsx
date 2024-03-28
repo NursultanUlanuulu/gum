97c77c36a79f82031366462d7b76201f95ad60bf
OrozbekDev04 <https://github.com/OrozbekDev04/zoone.git>
2024-03-28 15:49:16 +0600

    sale

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,15 @@
+import Sale from "../components/sale/Sale.jsx";
+import '../style/index.css'
+import {Routes, Route} from "react-router";
 const App = () => {
-  return <div>App</div>;
+
+  return <div>
+    <Routes>
+      <Route path="/" element={<Sale/>} />
+      <Route path="contact" element={<Sale /> } />
+      <Route path="page" element={<Sale />} />
+    </Routes>
+  </div>;
 };
 
 export default App;
---
+++ src/utils/constants/constants.js
@@ -0,0 +1,57 @@
+import img_1 from "../../assets/img.png"
+import InputBtn from "../../assets/svg/InputBtn.jsx";
+import Start from "../../assets/svg/start.jsx";
+
+export const  sale = [
+    {
+        id:1,
+        image:img_1,
+        pGreen:'В наличии',
+        // svgGreen:<InputBtn/>,
+        BigPP:'Беговая дорожка CardioPower S35',
+        reiting: 'Рейтинг',
+        // star:<Start/>,
+        sail:'1 134 900 ₽',
+    },
+    {
+        id:1,
+        image:img_1,
+        pGreen:'В наличии',
+        // svgGreen:<InputBtn/>,
+        BigPP:'Беговая дорожка CardioPower S35',
+        reiting: 'Рейтинг',
+        // star:<Start/>,
+        sail:'1 134 900 ₽',
+    },
+    {
+        id:1,
+        image:{img_1},
+        pGreen:'В наличии',
+        // svgGreen:<InputBtn/>,
+        BigPP:'Беговая дорожка CardioPower S35',
+        reiting: 'Рейтинг',
+        // star:<Start/>,
+        sail:'1 134 900 ₽',
+    },
+    {
+        id:1,
+        image:img_1,
+        pGreen:'В наличии',
+        // svgGreen:<InputBtn/>,
+        BigPP:'Беговая дорожка CardioPower S35',
+        reiting: 'Рейтинг',
+        // star:<Start/>,
+        sail:'1 134 900 ₽',
+    },
+    {
+        id:1,
+        image:img_1,
+        pGreen:'В наличии',
+        // svgGreen:<InputBtn/>,
+        BigPP:'Беговая дорожка CardioPower S35',
+        reiting: 'Рейтинг',
+        // star:<Start/>,
+        sail:'1 134 900 ₽',
+    },
+
+]
